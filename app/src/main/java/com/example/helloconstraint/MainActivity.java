bb9405550a3ae6be646100300f650146e3cb2919
ujjal <kozuuzol@gmail.com>
2021-03-01 10:02:05 +0545

    Homework lab2 commit3

--- app/src/main/java/com/example/helloconstraint/MainActivity.java
+++ app/src/main/java/com/example/helloconstraint/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnToast;
     private Button btnCount;
     private TextView Countt;
+    private Button btnZero;
     private int count=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btnToast=findViewById(R.id.button2);
         btnCount=findViewById(R.id.button3);
+        btnZero=findViewById(R.id.buttonZero);
         Countt=findViewById(R.id.textView2);
 
         btnToast.setOnClickListener(new View.OnClickListener(){
@@ -34,8 +36,26 @@ public class MainActivity extends AppCompatActivity {
 
                 count = count + 1;
                 Countt.setText("" + count);
+                btnZero.setBackgroundColor(getResources().getColor(R.color.Green33));
+
+                if(count%2==0) {
+                    btnCount.setBackgroundColor(getResources().getColor(R.color.Black00));
+                }
+
+                if(count%2==1) {
+                    btnCount.setBackgroundColor(getResources().getColor(R.color.FireRed));
+                }
+            }
+        });
+        btnZero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count = 0;
+                Countt.setText("" + count);
+                btnZero.setBackgroundColor(getResources().getColor(R.color.gray));
+                btnCount.setBackgroundColor(getResources().getColor(R.color.purple_700));
             }
         });
 
-            }
+    }
 }
